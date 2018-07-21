3b80169137c08e6863ba81d19732099cbfa9fa5c
John Tomaselli <jrt@gtz.com>
2018-07-21 11:26:41 -0400

    2a

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ export class App {
     config.title = 'Aurelia Babel SystemJS';
     config.map([
       {"route": ["", "welcome"], "name": "welcome", "moduleId": "./views/welcome/welcome", "nav": true, "title": "Welcome"},
-      {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"}
+      {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"},
+      {"route": "bootstrap-select", "name": "bootstrap-select-demo", "moduleId": "./views/bootstrap-select-demo/bootstrap-select", "nav": true, "title": "Bootstrap Select Demo"}
     ]);
 
     this.router = router;
