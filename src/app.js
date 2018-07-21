102e12b5a7dfb6a8775e1621e42e7c280a019da9
John Tomaselli <jrt@gtz.com>
2018-07-21 11:48:27 -0400

    2e

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ export class App {
     config.map([
       {"route": ["", "welcome"], "name": "welcome", "moduleId": "./views/welcome/welcome", "nav": true, "title": "Welcome"},
       {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"},
-      {"route": "bootstrap-select-demo", "name": "bootstrap-select-demo", "moduleId": "./views/bootstrap-select-demo/bootstrap-select", "nav": true, "title": "Bootstrap Select Demo"}
+      {"route": "bootstrap-select-demo", "name": "bootstrap-select-demo", "moduleId": "./views/bootstrap-select-demo/bootstrap-select-demo", "nav": true, "title": "Bootstrap Select Demo"}
     ]);
 
     this.router = router;
