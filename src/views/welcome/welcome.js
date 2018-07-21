aa6a09b25c5bf6203d02f443c744deec9edcb342
John Tomaselli <jrt@gtz.com>
2018-07-21 11:33:07 -0400

    2b

--- src/views/welcome/welcome.js
+++ src/views/welcome/welcome.js
@@ -3,7 +3,7 @@ export class Welcome {
   
   constructor() {
 //  let nDate = moment().format('MM/DD/YYYY')
-    this.heading = '1 - Welcome to the Aurelia Navigation App - 2a '// + nDate
+    this.heading = '1 - Welcome to the Aurelia Navigation App - 2b '// + nDate
   }
 
 }
